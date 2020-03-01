9c7f10328586d110fad85548f17c48427c0510e2
luaifei <lu.aifei@thoughtworks.com>
2020-03-01 20:00:32 +0800

    Code Smell [纯数据类]： 1. 将updateStatus的具体逻辑转移到Inventory类中，符合单一职责。

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Inventory[] inventories;
 
     public GildedRose(Inventory[] inventories) {
@@ -12,45 +9,7 @@ class GildedRose {
 
     public void updateStatus() {
         for (Inventory inventory : inventories) {
-            if (!inventory.name.equals(AGED_BRIE) && !inventory.name.equals(BACKSTAGE)) {
-                if (inventory.quality > 0 && !inventory.name.equals(SULFURAS)) {
-                    inventory.quality = inventory.quality - 1;
-                }
-            } else {
-                if (inventory.quality < 50) {
-                    inventory.quality = inventory.quality + 1;
-                }
-
-                if (inventory.name.equals(BACKSTAGE)) {
-                    if (inventory.sellIn < 11 && inventory.quality < 50) {
-                        inventory.quality = inventory.quality + 1;
-                    }
-
-                    if (inventory.sellIn < 6 && inventory.quality < 50) {
-                        inventory.quality = inventory.quality + 1;
-                    }
-                }
-            }
-
-            if (!inventory.name.equals(SULFURAS)) {
-                inventory.sellIn = inventory.sellIn - 1;
-            }
-
-            if (inventory.sellIn < 0) {
-                if (!inventory.name.equals(AGED_BRIE)) {
-                    if (!inventory.name.equals(BACKSTAGE)) {
-                        if (inventory.quality > 0 && !inventory.name.equals(SULFURAS)) {
-                            inventory.quality = inventory.quality - 1;
-                        }
-                    } else {
-                        inventory.quality = 0;
-                    }
-                } else {
-                    if (inventory.quality < 50) {
-                        inventory.quality = inventory.quality + 1;
-                    }
-                }
-            }
+            inventory.updateStatus();
         }
     }
 }
--- src/main/java/com/gildedrose/Inventory.java
+++ src/main/java/com/gildedrose/Inventory.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 public class Inventory {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     public String name;
 
@@ -17,5 +20,47 @@ public class Inventory {
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+    
+    public void updateStatus() {
+        if (!name.equals(AGED_BRIE) && !name.equals(BACKSTAGE)) {
+            if (quality > 0 && !name.equals(SULFURAS)) {
+                quality = quality - 1;
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+
+            if (name.equals(BACKSTAGE)) {
+                if (sellIn < 11 && quality < 50) {
+                    quality = quality + 1;
+                }
+
+                if (sellIn < 6 && quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+
+        if (!name.equals(SULFURAS)) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals(AGED_BRIE)) {
+                if (!name.equals(BACKSTAGE)) {
+                    if (quality > 0 && !name.equals(SULFURAS)) {
+                        quality = quality - 1;
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
     }
 }
