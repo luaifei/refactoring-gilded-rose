807f86c68a1e9d86d3cb583137bad56a26a6db61
luaifei <lu.aifei@thoughtworks.com>
2020-03-01 20:10:09 +0800

    Code Smell [过长的函数]： 1. 将Inventory.updateStatus()拆分成3个小函数块。

--- src/main/java/com/gildedrose/Inventory.java
+++ src/main/java/com/gildedrose/Inventory.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Inventory {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
@@ -11,18 +14,20 @@ public class Inventory {
 
     public int quality;
 
-    public Inventory(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
     
     public void updateStatus() {
+        updateQualityBefore();
+
+        updateSellIn();
+
+        updateQualityAfter();
+    }
+
+    private void updateQualityBefore() {
         if (!name.equals(AGED_BRIE) && !name.equals(BACKSTAGE)) {
             if (quality > 0 && !name.equals(SULFURAS)) {
                 quality = quality - 1;
@@ -42,11 +47,15 @@ public class Inventory {
                 }
             }
         }
+    }
 
+    private void updateSellIn() {
         if (!name.equals(SULFURAS)) {
             sellIn = sellIn - 1;
         }
+    }
 
+    private void updateQualityAfter() {
         if (sellIn < 0) {
             if (!name.equals(AGED_BRIE)) {
                 if (!name.equals(BACKSTAGE)) {
@@ -63,4 +72,5 @@ public class Inventory {
             }
         }
     }
+
 }
